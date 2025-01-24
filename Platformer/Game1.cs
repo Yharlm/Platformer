bf72093b652da56b584f5c49259feb75b212b559
Peshk0 <petar.tonev.pp25@pgmtplovdiv.info>
2025-01-24 12:17:56 +0200

    new project

--- Platformer/Game1.cs
+++ Platformer/Game1.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Runtime.ConstrainedExecution;
 
 namespace Platformer
 {
     public class Game1 : Game
     {
+        player player = new player();
         Texture2D ballTexture;
         Texture2D wall_texture;
         Vector2 pos;
         Vector2 ball_pos;
+        Vector2 grid_pos;
         float ball_speed = 300f;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -35,7 +38,7 @@ namespace Platformer
 
             // TODO: use this.Content to load your game content here
             ballTexture = Content.Load<Texture2D>("ball");
-            wall_texture = Content.Load<Texture2D>("Wall");
+            wall_texture = Content.Load<Texture2D>("dirt");
         }
 
         protected override void Update(GameTime gameTime)
@@ -47,15 +50,41 @@ namespace Platformer
             float updatedBallSpeed = ball_speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             var keyboardState = Keyboard.GetState();
-
+            float size_diference = player.size*63;
             if (keyboardState.IsKeyDown(Keys.D))
             {
-                ball_pos.X += updatedBallSpeed;
+                ball_pos.X += 10f;
+                if (ball_pos.X % 100 == 0)
+                {
+                    grid_pos.X += size_diference;
+                }
             }
             if (keyboardState.IsKeyDown(Keys.A))
             {
-                ball_pos.X -= updatedBallSpeed;
+                ball_pos.X -= 10f;
+                if (ball_pos.X % 100 == 0)
+                {
+                    grid_pos.X -= size_diference;
+                }
             }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                ball_pos.Y += 10f;
+                if (ball_pos.Y % 100 == 0)
+                {
+                    grid_pos.Y += size_diference;
+                }
+            }
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                ball_pos.Y -= 10f;
+                if (ball_pos.Y % 100 == 0)
+                {
+                    grid_pos.Y -= size_diference;
+                }
+            }
+
+
             base.Update(gameTime);
         }
 
@@ -64,14 +93,20 @@ namespace Platformer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(ballTexture,ball_pos,Color.Red);
-            _spriteBatch.End();
+            
 
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(wall_texture, new Vector2(0, 222), Color.White);
+            _spriteBatch.Begin(SamplerState.PointClamp = );
+            for (int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    _spriteBatch.Draw(wall_texture, new Vector2(i * 100, j * 100), null, Color.White, 0f, Vector2.Zero, 7f, SpriteEffects.None, 0f);
+                }
+            }
             _spriteBatch.End();
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(ballTexture, new Vector2(grid_pos.X, grid_pos.Y), null, Color.Red, 0f, Vector2.Zero, player.size, SpriteEffects.None, 0f);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ Platformer/classes.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    internal class player
+    {
+        public Vector2 pos;
+        public Vector2 ball_pos;
+        public float player_speed = 300f;
+        public float size = 1f;
+        public Texture2D player_Texture2D;
+    }
+}
